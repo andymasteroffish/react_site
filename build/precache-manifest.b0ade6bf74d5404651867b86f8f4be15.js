3fa7b2e3e2bf562d7d3d425341f44d1122be6974
Andy Wallace <andymasteroffish@gmail.com>
2019-08-06 15:18:13 -0400

    jane did a bunch of work on video divs

---
+++ build/precache-manifest.b0ade6bf74d5404651867b86f8f4be15.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8043b229e27f0a407660c9974cd22708",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c8061dd46ea74b55b548",
+    "url": "/static/css/main.50e46e34.chunk.css"
+  },
+  {
+    "revision": "82e68885ac66aca82340",
+    "url": "/static/js/2.77f90329.chunk.js"
+  },
+  {
+    "revision": "c8061dd46ea74b55b548",
+    "url": "/static/js/main.1aae987f.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
